34cef82164650bab6988573ba5c85b9f32494997
samuel vermeuln <70477826+samuelvermeuln@users.noreply.github.com>
2021-12-21 00:02:26 -0300

    Login pronto

--- ignews/ignews/.next/static/chunks/webpack.js
+++ ignews/ignews/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "61941f6f06162834"; }
+/******/ 		__webpack_require__.h = function() { return "41cf4b3c419c833b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
