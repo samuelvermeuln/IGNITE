976eb8adda93497aa707500f4c3c499abdff0dbb
samuel vermeuln <70477826+samuelvermeuln@users.noreply.github.com>
2021-12-21 22:30:53 -0300

    conexão com o faunaDB

--- ignews/ignews/.next/static/chunks/webpack.js
+++ ignews/ignews/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "41cf4b3c419c833b"; }
+/******/ 		__webpack_require__.h = function() { return "4e514ea0fdd8eeb0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
